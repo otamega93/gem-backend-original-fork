e1b8f0c9304d9a8d4300e2a802f9d73ede13be2c
informatica <gomezcarlose@gmail.com>
2016-06-22 15:04:37 -0400

    First project real commit

---
+++ gem-backend/src/main/java/ve/com/gem/GemBackendApplication.java
@@ -0,0 +1,12 @@
+package ve.com.gem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemBackendApplication.class, args);
+	}
+}
---
+++ gem-backend/src/test/java/ve/com/gem/GemBackendApplicationTests.java
@@ -0,0 +1,18 @@
+package ve.com.gem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GemBackendApplication.class)
+@WebAppConfiguration
+public class GemBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
